380d1e951d5798041f730055672f677c279e9616
saliou-t <zalltalla46@gmail.com>
2021-07-10 12:13:40 +0000

    création des entités

---
+++ DevBankend_API-plateform/src/Entity/Chauffeur.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChauffeurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ChauffeurRepository::class)
+ */
+class Chauffeur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $permis;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cni;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPermis(): ?string
+    {
+        return $this->permis;
+    }
+
+    public function setPermis(string $permis): self
+    {
+        $this->permis = $permis;
+
+        return $this;
+    }
+
+    public function getCni(): ?string
+    {
+        return $this->cni;
+    }
+
+    public function setCni(string $cni): self
+    {
+        $this->cni = $cni;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
